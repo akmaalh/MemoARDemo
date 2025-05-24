d253bbd40841d43b8c128b30173bb38ef307aa81
akmaalh <akmalhakim2106@gmail.com>
2025-05-24 11:57:57 +0700

    change localization to id

--- MemoARDemo/MemoARDemoApp.swift
+++ MemoARDemo/MemoARDemoApp.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 @main
 struct MemoARDemoApp: App {
+    init() {
+        // Removed force AppleLanguages hack. Locale will be set via .environment modifier.
+    }
+    
     @StateObject private var dataManager = GameDataManager()
     @State private var showUserForm = false
     @State private var showTutorial = false
@@ -49,6 +53,7 @@ struct MemoARDemoApp: App {
                     }
                 }
             }
+            .environment(\.locale, Locale(identifier: "id"))
         }
     }
 }
